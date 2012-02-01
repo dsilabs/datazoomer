86cb94fd65dc590fa97e1664dd722b3def0267bf
Herb Lainchbury <herb@dynamic-solutions.com>
2012-01-31 23:16:59 -0800

    Eliminated lib directory

--- www/index.py
+++ www/index.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
-import sys, os
+try:
+    try:
+        import zoom
+        zoom.run()
 
-lib_path = os.path.abspath('../lib')
-sys.path = [lib_path] + sys.path
+    except ImportError:
+        # Development environment
+        import sys, os, imp
+        lib_path = os.path.abspath('..')
+        sys.path = [lib_path] + sys.path
+        import zoom
+        zoom.run()
 
-import zoom
-zoom.run()
+except SystemExit:
+    pass
+
+except:
+    import traceback
+    print 'Content-type: text/html\n\n'
+    print '<pre>%s</pre>' % traceback.format_exc()
 
