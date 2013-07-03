1467763c27bbd4204551808ab8f3bb9c1f3e95e3
Herb Lainchbury <herb@dynamic-solutions.com>
2013-07-03 00:23:52 -0700

    Added markdown version info

--- apps/info/python.py
+++ apps/info/python.py
@@ -1,11 +1,18 @@
 
 from zoom import *
 import sys
+import markdown
 
 def view():
 
     items = {}
     items['version'] = sys.version
+
+    try:
+        items['markdown version'] = markdown.__version__.version
+    except:
+        items['markdown version'] = 'unable to get version info'
+
     items['path'] = '<br>'.join(sys.path)
     items['platform'] = sys.platform
     
