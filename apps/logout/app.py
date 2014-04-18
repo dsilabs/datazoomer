1173112b4cd496f1a457320eeb37bfb7eace02b2
Herb Lainchbury <herb@dynamic-solutions.com>
2014-04-18 08:40:27 -0400

    Log successful logouts

--- apps/logout/app.py
+++ apps/logout/app.py
@@ -1,11 +1,12 @@
 
-from zoom import user, redirect_to, warning
+from zoom import user, redirect_to, warning,  logger
 
 def app():
     if not user.is_authenticated:
         warning('You are not logged in')
     else:
         user.logout()
+        logger.info('user successfully logged out')
     return redirect_to('/')
 
 
