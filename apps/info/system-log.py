0931bc2a2cbb4557827b35b867eef1cad6c88853
Herb Lainchbury <herb@dynamic-solutions.com>
2014-04-18 09:28:27 -0400

    Made all log entry details visible

--- apps/info/system-log.py
+++ apps/info/system-log.py
@@ -4,12 +4,6 @@ from zoom import *
 db = system.database
 
 def view(id='', status=None):
-    def error_link(id, status):
-        if status in ['E','S','W']:
-            new_route = route[1:] + [str(id)]
-            return link_to(status, *new_route)
-        return status
-
     if id:
         data = db('select * from log where id=%s and server=%s order by timestamp desc limit 100', id, request.server)
         if len(data) == 1:
@@ -17,23 +11,23 @@ def view(id='', status=None):
             for field in data[0].column_names():
                  result.append((field, '<pre>%s</pre>' % data[0].value_by_name(field)),)
             labels = ['Column', 'Value']
-            return Page('<H1>Error Details</H1>%s' % browse(result, labels=labels))
+            return Page('<H1>Log Entry</H1>%s' % browse(result, labels=labels))
 
     if status:
         data = db('select * from log where server=%s and status=%s order by timestamp desc limit 100',request.server,status)
     else:
         data = db('select * from log where server=%s order by timestamp desc limit 100',request.server)
 
-    labels = ['ID','User','Address','Route','Status','Elapsed','Timestamp','How Long Ago']
+    labels = ['ID','User','Address','Route','Status','Elapsed','How Long Ago','Message']
     items = [(
-        i.id,
+        link_to(i.id, route[-1], i.id),
         i.user,
         i.address,
         i.route,
-        error_link(i.id, i.status),
+        i.status,
         i.elapsed,
-        i.timestamp,
-        how_long_ago(i.timestamp),
+        '<span title="%s">%s</span>' % (i.timestamp, how_long_ago(i.timestamp)),
+        i.message[:30]
         ) for i in data]
     content = browse(items,labels=labels)
     return page(content, title='System Log')
