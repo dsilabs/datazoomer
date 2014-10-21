6933c117b5d829ae41f56896fbcf91930dae7b7a
Herb Lainchbury <herb@dynamic-solutions.com>
2014-10-20 23:38:23 -0700

    Added errors page

--- apps/info/app.py
+++ apps/info/app.py
@@ -5,6 +5,7 @@ app = App()
 system.app.menu = [
     ('index','Overview','index'),
     ('system-log','System Log','system-log'),
+    ('errors','Errors','errors'),
     ('top-users','Top Users','top-users'),
     ('addresses','Addresses','addresses'),
     ('tables','Tables','tables'),
---
+++ apps/info/errors.py
@@ -0,0 +1,35 @@
+
+from zoom import *
+
+db = system.database
+
+def view(id='', status=None):
+    if id:
+        data = db('select * from log where id=%s and server=%s order by timestamp desc limit 100', id, request.server)
+        if len(data) == 1:
+            result = []
+            for field in data[0].column_names():
+                 result.append((field, '<pre>%s</pre>' % data[0].value_by_name(field)),)
+            labels = ['Column', 'Value']
+            return Page('<H1>Errors</H1>%s' % browse(result, labels=labels))
+
+    if status:
+        data = db('select * from log where server=%s and status=%s order by timestamp desc limit 100',request.server,status)
+    else:
+        data = db('select * from log where server=%s and status="E" order by timestamp desc limit 100',request.server)
+
+    labels = ['ID','User','Address','Route','Status','Elapsed','How Long Ago','Message']
+    items = [(
+        link_to(i.id, route[-1], i.id),
+        i.user,
+        i.address,
+        i.route,
+        i.status,
+        i.elapsed,
+        '<span title="%s">%s</span>' % (i.timestamp, how_long_ago(i.timestamp)),
+        i.message[:30]
+        ) for i in data]
+    content = browse(items,labels=labels)
+    return page(content, title='Errors')
+    
+        
