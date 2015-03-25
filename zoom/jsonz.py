8d63a6b368db738ee243b9c6b16b9f16b9a7962c
Sean Hayes <sean@dynamic-solutions.com>
2015-03-25 21:14:10 +0000

    Add Decimal support to json encode/decode for json version <2.1

--- zoom/jsonz.py
+++ zoom/jsonz.py
@@ -6,6 +6,11 @@
 
 import json
 import datetime
+from decimal import Decimal
+
+# simplejson (part of python 2.7+) supports Decimal as of v2.1
+json_version = map(int,json.__version__.split('.')[:2])
+json_support_decimal = json_version[0]>=2 and json_version[1]>=1
 
 def loads(text):
     def dhandler(obj):
@@ -15,6 +20,8 @@ def loads(text):
                 return datetime.datetime.strptime(obj['value'], '%Y-%m-%dT%H:%M:%S.%f')
             elif t == 'date':
                 return datetime.datetime.strptime(obj['value'], '%Y-%m-%d').date()
+            elif t == 'decimal':
+                return Decimal(str(obj['value']))
         return obj                
     return json.loads(text, object_hook=dhandler)
     
@@ -26,6 +33,8 @@ def dumps(data, *a, **k):
             return dict(__type__='datetime',value=obj.isoformat())
         elif isinstance(obj, datetime.date):
             return dict(__type__='date',value=obj.isoformat())
+        elif isinstance(obj, Decimal) and not json_support_decimal:
+            return dict(__type__='decimal', value=float(str(obj)))
         else:
             raise TypeError, 'Object of type %s with value %s is not JSON serializable.' % (type(obj),repr(obj))
     return json.dumps(data, default=handler, *a, **k)
@@ -53,22 +62,16 @@ if __name__ == '__main__':
             dj = dumps(d)
             d2 = loads(dj)
             self.assertEqual(d,d2)
+
+        def test_decimal(self):
+            d = [22.32, u"22.32", Decimal('22.32'), Decimal(22.32)]
+            exp = '[22.32, "22.32", {"__type__": "decimal", "value": 22.32}, {"__type__": "decimal", "value": 22.32}]'
+            dj = dumps(d)
+            d2 = loads(dj)
+            if json_support_decimal:
+                # otherwise ths will fail or can we determine what "type" the Decimal was constructed with?
+                self.assertEqual(d,d2)
+            if not json_support_decimal: self.assertEqual(dj, exp)
             
     unittest.main()
-    #d = datetime.date.today()
-    #print d
-    #print dumps(d), type(dumps(d))
-    #print loads(dumps(d)),type(loads(dumps(d)))
-                    
-    #d = datetime.datetime.now()
-    #print d
-    #print dumps(d), type(dumps(d))
-    #print loads(dumps(d)),type(loads(dumps(d)))
-    
-    
-    
-    
-    
-    
-    
-    
+
