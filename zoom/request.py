dd9eb8696430298af66dff7c89377d234e16bbab
Herb Lainchbury <herb@dynamic-solutions.com>
2015-05-18 16:28:54 -0700

    Added cookies and other bits to request object

--- zoom/request.py
+++ zoom/request.py
@@ -15,48 +15,27 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, cgi, sys
+import os
+import sys
+import cgi
 import urllib
+import Cookie
 from types import ListType
 
-env = os.environ
 
 def calc_domain(host):
     if host:
         return host.split(':')[-1:][0].split('www.')[-1:][0]
     return ''
 
-class Request:
-    def __init__(self):    
 
-        path = urllib.quote(env.get('PATH_INFO', env.get('REQUEST_URI','').split('?')[0]))
-        route = path != '/' and path.split('/')[1:] or []
+def get_cookies(raw_cookie):
+    cookie = Cookie.SimpleCookie(raw_cookie)
+    return dict([(k,cookie[k].value) for k in cookie])
 
-        # gather some commonly required environment variables
-        request = dict(
-            path   = path,
-            host   = env.get('HTTP_HOST'),
-            domain = calc_domain(env.get('HTTP_HOST')),
-            uri    = env.get('REQUEST_URI','index.py'),
-            query  = env.get('QUERY_STRING'),
-            ip     = env.get('REMOTE_ADDR'),
-            user   = env.get('REMOTE_USER'),
-            port   = env.get('SERVER_PORT'),
-            server = env.get('SERVER_NAME','localhost'),
-            script = env.get('SCRIPT_FILENAME'),
-            agent  = env.get('HTTP_USER_AGENT'),
-            method = env.get('REQUEST_METHOD'),
-            protocol = env.get('HTTPS','off') == 'on' and 'https' or 'http',
-            referrer = env.get('HTTP_REFERER'),
-            route = route,
-            )
-        self.__dict__ = request  
-        
-    def __str__(self):
-        return '{\n%s\n}' % '\n'.join( '  %s:%s' % (key,self.__dict__[key]) for key in self.__dict__ )       
 
 class Webvars:
-    def __init__(self):
+    def __init__(self, env=os.environ):
         """gather query fields"""
 
         # switch to binary mode on windows systems
@@ -102,10 +81,47 @@ class Webvars:
         return str(self)
 
 
+class Request:
+    def __init__(self, env=os.environ):
+
+        path = urllib.quote(env.get('PATH_INFO', env.get('REQUEST_URI','').split('?')[0]))
+        route = path != '/' and path.split('/')[1:] or []
+
+        # gather some commonly required environment variables
+        request = dict(
+            path = path,
+            host = env.get('HTTP_HOST'),
+            domain = calc_domain(env.get('HTTP_HOST')),
+            uri = env.get('REQUEST_URI','index.py'),
+            query = env.get('QUERY_STRING'),
+            ip = env.get('REMOTE_ADDR'),
+            user = env.get('REMOTE_USER'),
+            port = env.get('SERVER_PORT'),
+            server = env.get('SERVER_NAME','localhost'),
+            script = env.get('SCRIPT_FILENAME'),
+            home = os.path.dirname(env.get('SCRIPT_FILENAME','')),
+            agent  = env.get('HTTP_USER_AGENT'),
+            method = env.get('REQUEST_METHOD'),
+            module = env.get('mod_wsgi.version',None) and 'wsgi' or 'cgi',
+            mode = env.get('mod_wsgi.process_group', None) and 'daemon' or 'embedded',
+            protocol = env.get('HTTPS','off') == 'on' and 'https' or 'http',
+            referrer = env.get('HTTP_REFERER'),
+            cookies  = get_cookies(env.get('HTTP_COOKIE')),
+            route = route,
+            data = Webvars(env).__dict__,
+            )
+        self.__dict__ = request
+
+    def __str__(self):
+        return '{\n%s\n}' % '\n'.join('  %s:%s' % 
+                (key,self.__dict__[key]) for key in self.__dict__)
+
+
 request = Request()
 webvars = Webvars()
-data    = webvars.__dict__
+data    = request.data
 route   = request.route
+
 
 if __name__ == '__main__':
     import unittest
