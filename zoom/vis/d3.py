2d74ea09ad37327dfe9ccf0998737d15122b3447
Sean Hayes <sean@dynamic-solutions.com>
2015-08-13 06:28:45 +0000

    Add d3 calendar

--- zoom/vis/d3.py
+++ zoom/vis/d3.py
@@ -33,3 +33,38 @@ class scatter(object):
         system.tail = system.tail | [self._declare_ % (locals())]
         return ''
 
+class calendar(object):
+    """d3.js calendar plot"""
+    _declare_ = """
+    <script>
+        var %(ref)s = dsi.calendar();
+        d3.json("%(view)s", function(data) {
+            d3.select("%(selector)s")
+              .datum(data)
+              .call(%(ref)s);
+        });
+
+        d3.select("#description p.dropdown.brewer select")
+            .on("click", function() {
+                var p = d3.select(this).select(":checked").text();
+                d3.select("%(selector)s").selectAll("svg")
+                    .attr("class", p);
+            });
+    </script>
+    """
+    name = property(lambda self: '%s_%s' % (self.__class__.__name__, id(self)))
+    ref = property(lambda self: self.name, doc="the reference to the object")
+    libs = ["/static/dz/d3/lib/tip/d3.tip.js", "/static/dz/d3/lib/colorbrewer/colorbrewer.js"]
+
+    def __init__(self, view_for_data, selector="chart"):
+        self.view = view_for_data
+        self.selector = selector.startswith('#') and selector or '#{}'.format(selector)
+    def __str__(self):
+        libs = d3_libs + self.libs
+        system.libs = system.libs | libs
+        system.styles = system.styles | ['/static/dz/d3/lib/colorbrewer/colorbrewer.css']
+        ref = self.ref
+        view = self.view
+        selector = self.selector
+        system.tail = system.tail | [self._declare_ % (locals())]
+        return ''
