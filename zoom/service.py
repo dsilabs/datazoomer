a5733149699e739210c2f48c06c974f3a5ee07d2
Herb Lainchbury <herb@dynamic-solutions.com>
2015-10-27 13:37:03 -0700

    added logging, scanning

--- zoom/service.py
+++ zoom/service.py
@@ -3,6 +3,19 @@ import db
 import os
 import time
 import sys
+import logging
+from subprocess import Popen, PIPE, call
+
+con_formatter = logging.Formatter('%(asctime)s  %(name)-15s %(levelname)-8s %(message)s')
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(con_formatter)
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.DEBUG)
+root_logger.addHandler(console_handler)
+
+logger = logging.getLogger('service')
+#logger.setLevel(logging.DEBUG)
 
 sleep_time = 0.25
 
@@ -11,15 +24,49 @@ def emit(t):
     sys.stdout.flush()
 
 def perform(jobs_path):
-    emit('tick\n')
+    def run_main(fn):
+        path, name = os.path.split(fn)
+        try:
+            save_dir = os.getcwd()
+            try:
+                os.chdir(path)
+                if not name.endswith('.py'):
+                    call(['./'+name])
+                else:
+                    call(['python',name])
+            finally:
+                os.chdir(save_dir)
+        except:
+            logger.debug('an error occured in %s' % fn)
+
+            t = traceback.format_exc()
+
+            emit('*' * 40 + '\n')
+            emit('Exception occured in %s\n' % pathname)
+            emit(t)
+            emit('*' * 40 + '\n')
+
+            error_file = os.path.join(path, ERROR_FILE)
+            f = open(error_file,'w')
+            f.write(t)
+            f.close()
+        finally:
+            logger.debug('ran %s' % path)
+
     for filename in os.listdir(jobs_path):
-        pass
+        pathname = os.path.abspath(os.path.join(jobs_path, filename))
+        if os.path.isdir(pathname):
+            fn = os.path.join(pathname, 'service.py')
+            if os.path.exists(fn):
+                #emit(pathname + '\n')
+                run_main(fn)
+
     import random
-    return random.randint(0,1)
+    return random.randint(0,3)
 
 def process(jobs_path, time_to_stop=False):
     def sleep(t):
-        emit('sleeping\n')
+        #emit('sleeping\n')
         time.sleep(t)
 
     done = False
@@ -30,7 +77,8 @@ def process(jobs_path, time_to_stop=False):
         first_time = False
 
         n = perform(jobs_path)
-        emit('{} jobs performed\n'.format(n))
+        #if n:
+        #    emit('{} jobs performed\n'.format(n))
 
         if callable(time_to_stop):
             done = time_to_stop()
@@ -48,7 +96,9 @@ if __name__ == '__main__':
         return lambda: time.time() > stop_time
 
     jobs_path = '/home/herb/work/services'
-    process(jobs_path, quit_in(1))
+    try:
+        process(jobs_path, quit_in(2))
+    except KeyboardInterrupt:
+        pass
 
-    print 'ok'
 
