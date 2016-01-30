bfb5bd30af8b407ce72727b54a5c2e1fe4efb590
Herb Lainchbury <herb@dynamic-solutions.com>
2016-01-29 22:29:59 -0800

    remove commented out database logging code

--- zoom/startup.py
+++ zoom/startup.py
@@ -107,12 +107,7 @@ def generate_response(instance_path):
                 if profiler:
                     profiler.enable()
 
-                #if system.settings.get('application_database_log'):
-                #    system.db('SET GLOBAL general_log = "ON"')
-
                 response = system.app.run()
-
-                #system.db('SET GLOBAL general_log = "OFF"')
 
                 if profiler:
                     profiler.disable()
