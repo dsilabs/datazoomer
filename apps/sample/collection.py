5d373e16bc7a0622174cfaff16419acac02fcd20
Herb Lainchbury <herb@dynamic-solutions.com>
2016-02-03 09:04:07 -0800

    add RadioField and validators to sample collection

--- apps/sample/collection.py
+++ apps/sample/collection.py
@@ -10,11 +10,19 @@ import zoom.collect
 class SampleContact(zoom.collect.CollectionRecord): pass # the app specific part
 Contact = SampleContact # the nickname
 
+statuses = [
+    ('New', 'N'),
+    ('Open', 'O'),
+    ('Pending', 'P'),
+    ('Closed', 'C'),
+]
+
 # define the fields (always include a name field)
 contact_fields = Fields(
     TextField('Name', required, maxlength=80),
-    TextField('Title'),
+    TextField('Title', required, MinimumLength(3)),
     DateField('Date', format='%A %b %d, %Y'),
+    RadioField('Status', values=statuses),
     EditField('Notes'),
 )
 
