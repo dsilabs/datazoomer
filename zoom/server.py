d1c700d8ea5f9dffd5e1e336bab0837e118e3323
Herb Lainchbury <herb@dynamic-solutions.com>
2016-05-20 08:28:07 -0700

    remove scaffolding

--- zoom/server.py
+++ zoom/server.py
@@ -1,53 +1,57 @@
 """
     portable server
 
+    runs an instance of DataZoomer using the builtin Python WSGI server.
+
     >>> server = WSGIApplication()
 """
 
 import sys
-from request import Request
-import middleware
 from wsgiref.simple_server import make_server
-from middleware import handle
+from timeit import default_timer as timer
 
+from zoom.request import Request
+import zoom.middleware
 
-#----------- WSGI application
-#def application(environ, start_response):
-    #return WSGIApplication()(environ, start_response)
+def reset_modules():
+    """reset the modules to a known starting set
+
+    memorizes the modules currently in use and then removes any other
+    modules when called again"""
+    global init_modules
+    if globals().has_key('init_modules'):
+        for module in [x for x in sys.modules.keys() if x not in init_modules]:
+            del sys.modules[module]
+    else:
+        init_modules = sys.modules.keys()
 
 
 class WSGIApplication(object):
+    """a WSGI Application wrapper for DataZoomer
+    """
     def __init__(self, instance='.', handlers=None):
         self.handlers = handlers
         self.instance = instance
 
     def __call__(self, environ, start_response):
-        request = Request(environ, self.instance)
+        reset_modules()
+        start_time = timer()
+        request = Request(environ, self.instance, start_time)
         status, headers, content = middleware.handle(
-                request,
-                self.handlers,
-                )
+            request,
+            self.handlers,
+        )
         start_response(status, headers)
         return [content]
 
-def run_once(port=8004, instance=None):
-    done = False
-    try:
-        while not done:
-            server = make_server('', port, WSGIApplication(instance))
-            try:
-                server.handle_request()
-            finally:
-                del server
-            done = True
-    except KeyboardInterrupt:
-        print('done')
 
 def run(port=8004, instance=None):
+    """run DataZoomer using internal HTTP Server
+
+    The instance variable is the path of the directory on the system where the
+    sites folder is located. (e.g. /work/web)
+    """
     server = make_server('', port, WSGIApplication(instance))
     server.serve_forever()
 
-if __name__ == '__main__':
-    port = 8004
-    instance = '/home/herb/work/web'
-    run(port, instance)
+
