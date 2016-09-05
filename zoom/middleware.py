353b5bf821cc80bbe049ae46ed34bf4ecfab72ba
Herb Lainchbury <herb@dynamic-solutions.com>
2016-09-05 10:17:06 -0700

    clean up middleware module

--- zoom/middleware.py
+++ zoom/middleware.py
@@ -6,33 +6,42 @@
     such as content serving, caching, error trapping, security, etc..
 """
 
+# pylint: disable=broad-except
+# We sometimes catch anything that hasn't already been handled and provide a
+# useful respose to the browser.  That's not usually advised but in this
+# case it's what we want.
+
 import os
 import sys
 import traceback
 import json
-import db
-import config
 from StringIO import StringIO
-from response import PNGResponse, JPGResponse, HTMLResponse, CSSResponse, JavascriptResponse
+
+from .response import (
+    PNGResponse, JPGResponse, CSSResponse, JavascriptResponse
+)
 
 
-form = """<br><br>
+SAMPLE_FORM = """<br><br>
 <form action="" id="dz_form" name="dz_form" method="POST" enctype="multipart/form-data">
     first name<input name="first_name" value="" type="text">
     last name<input name="last_name" value="" type="text">
     picture<input name="photo" value="" type="file">
     <input style="" name="send_button" value="send" class="button" type="submit" id="send_button">
 </form>
-""".replace('/n','<br>')
+""".replace('/n', '<br>')
 
 
 def debug(request):
     """fake app for development purposes"""
 
-    def formatr(title, content):
-        return '<pre>\n====== %s ======\n%s\n</pre>' % (title, content)
-    def format(title, content):
+    def format_section(title, content):
+        """format a section for debugging output"""
         return '<pre>\n====== %s ======\n%s\n</pre>' % (title, repr(content))
+
+    def formatr(title, content):
+        """format a section for debugging output in raw form"""
+        return '<pre>\n====== %s ======\n%s\n</pre>' % (title, content)
 
     content = ''
 
@@ -49,24 +58,26 @@ def debug(request):
         content += formatr('printed output', '{printed_output}')
 
         content += formatr('request', request)
-        content += formatr('form', form)
+        content += formatr('form', SAMPLE_FORM)
         content += formatr('paths', json.dumps(dict(
             path=[sys.path],
             directory=os.path.abspath('.'),
             pathname=__file__,
                 ), indent=2))
-        content += formatr('environment', json.dumps(os.environ.items(), indent=2))
+        content += formatr('environment',
+                           json.dumps(os.environ.items(), indent=2))
 
         print 'testing printed output'
 
-        if 'photo' in request.data and request.data['photo'].filename:
-            content += format('filename',request.data['photo'].filename)
-            content += format('filedata',request.data['photo'].value)
+        data = request.data
+        if 'photo' in data and data['photo'].filename:
+            content += format_section('filename', data['photo'].filename)
+            content += format_section('filedata', data['photo'].value)
 
         headers = [('Content-type', 'text/html'),
                    ('Content-Length', str(len(content)))]
 
-    except:
+    except Exception:
         status = '200 OK'
         content = traceback.format_exc()
         headers = [('Content-type', 'text/plain'),
@@ -76,6 +87,7 @@ def debug(request):
 
 
 def app(request):
+    """Call the main Application"""
     from zoom.startup import run_as_app
     response = run_as_app(request)
     doc = response.render_doc()
@@ -85,6 +97,7 @@ def app(request):
 
 
 def capture_stdout(request, handler, *rest):
+    """Capture printed output for debugging purposes"""
     real_stdout = sys.stdout
     sys.stdout = StringIO()
     try:
@@ -98,6 +111,7 @@ def capture_stdout(request, handler, *rest):
 
 
 def serve_response(filename):
+    """Serve up various respones with their correct response type"""
     known_types = dict(
             png=PNGResponse,
             jpg=JPGResponse,
@@ -107,9 +121,9 @@ def serve_response(filename):
             js=JavascriptResponse,
             )
     if os.path.exists(filename):
-        fn = filename.lower()
+        func = filename.lower()
         for file_type in known_types:
-            if fn.endswith('.'+file_type):
+            if func.endswith('.' + file_type):
                 data = open(filename).read()
                 response = known_types[file_type](data)
                 return response.render_wsgi()
@@ -119,6 +133,7 @@ def serve_response(filename):
 
 
 def serve_static(request, handler, *rest):
+    """Serve a static file"""
     if request.path.startswith('/static/'):
         root_dir = request.instance
         filename = os.path.join(root_dir, 'www', request.path[1:])
@@ -128,6 +143,7 @@ def serve_static(request, handler, *rest):
 
 
 def serve_themes(request, handler, *rest):
+    """Serve a theme file"""
     if request.path.startswith('/themes/'):
         root_dir = request.instance
         filename = os.path.join(root_dir, request.path[1:])
@@ -137,6 +153,7 @@ def serve_themes(request, handler, *rest):
 
 
 def serve_images(request, handler, *rest):
+    """Serve an image file"""
     if request.path.startswith('/images/'):
         filename = os.path.join(request.root, 'content', request.path[1:])
         return serve_response(filename)
@@ -145,6 +162,7 @@ def serve_images(request, handler, *rest):
 
 
 def serve_favicon(request, handler, *rest):
+    """Serve a favicon file"""
     if request.path == '/favicon.ico':
         root_dir = request.root
         filename = os.path.join(root_dir, 'static', 'images', request.path[1:])
@@ -154,6 +172,7 @@ def serve_favicon(request, handler, *rest):
 
 
 def serve_html(request, handler, *rest):
+    """Direct a request for an HTML page to the content app"""
     if request.path.endswith('.html'):
         request.path = '/content' + request.path[:-5]
         request.route = request.path.split('/')[1:]
@@ -163,9 +182,10 @@ def serve_html(request, handler, *rest):
 
 
 def trap_errors(request, handler, *rest):
+    """Trap exceptions and raise a server error"""
     try:
         return handler(request, *rest)
-    except:
+    except Exception:
         status = '500 Internal Server Error'
         content = traceback.format_exc()
         headers = [('Content-type', 'text/plain'),
@@ -174,10 +194,12 @@ def trap_errors(request, handler, *rest):
 
 
 def _handle(request, handler, *rest):
+    """invoke the next handler"""
     return handler(request, *rest)
 
 
 def handle(request, handlers=None):
+    """handle a request"""
     default_handlers = (
         trap_errors,
         serve_favicon,
@@ -192,7 +214,7 @@ def handle(request, handlers=None):
     return _handle(request, *(handlers or default_handlers))
 
 
-debug_handlers = (
+DEBUGGING_HANDLERS = (
     trap_errors,
     serve_favicon,
     serve_static,
