ebf41705866f52a60dedc379f06b5058499e9df9
Herb Lainchbury <herb@dynamic-solutions.com>
2016-09-05 07:21:30 -0700

    clean up cookies module

--- zoom/cookies.py
+++ zoom/cookies.py
@@ -1,7 +1,7 @@
-"""
-    cookies.py
+# -*- coding: utf-8 -*-
 
-    datazoomer cookie routines
+"""
+    zoom.cookies
 
     The following test is a bit lame because the timestamp makes it tricky to
     test.  Ideally we'd pass in a timer somehow but the cookie module is
@@ -17,8 +17,6 @@
     True
     >>> len(v)
     69
-
-
 """
 
 import Cookie
@@ -26,37 +24,40 @@ import Cookie
 
 SESSION_COOKIE_NAME = 'dz4sid'
 SUBJECT_COOKIE_NAME = 'dz4sub'
+ONE_YEAR = 365 * 24 * 60 * 60
 
 
 def get_cookies(raw_cookie):
+    """extract cookies from raw cookie data"""
     cookie = Cookie.SimpleCookie(raw_cookie)
     result = dict([(k, cookie[k].value) for k in cookie])
     return result
 
-
 def make_cookie():
+    """construct a cookie"""
     return Cookie.SimpleCookie()
 
-
 def add_value(cookie, name, value, lifespan, secure):
+    """add a value to a cookie"""
     cookie[name] = value
     cookie[name]['httponly'] = True
     cookie[name]['expires'] = lifespan # in seconds
     if secure:
         cookie[name]['secure'] = True
 
-
 def get_value(cookie):
-    _, v = str(cookie).split(': ', 1)
-    return v
-
+    """get the value portion of a cookie"""
+    _, value = str(cookie).split(': ', 1)
+    return value
 
 def set_session_cookie(response, session_id, subject, lifespan, secure=True):
-    ONE_YEAR = 365 * 24 * 60 * 60
+    """construct a session cookie"""
     cookie = make_cookie()
     add_value(cookie, SESSION_COOKIE_NAME, session_id, lifespan, secure)
     add_value(cookie, SUBJECT_COOKIE_NAME, subject, ONE_YEAR, secure)
-    k,v = str(cookie).split(': ',1)
-    v = v.replace('\r',' ').replace('\n',' ') # mod_wsgi doesn't like newlines
-    response.headers[k] = v
+    key, value = str(cookie).split(': ', 1)
 
+    # mod_wsgi doesn't like newlines
+    value = value.replace('\r', ' ').replace('\n', ' ')
+
+    response.headers[key] = value
