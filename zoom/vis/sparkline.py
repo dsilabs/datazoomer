77ca4db2fe86db7161b61b2d126dd2f858f1f312
Herb Lainchbury <herb@dynamic-solutions.com>
2016-12-14 14:10:14 -0800

    partially update sparkline to use components

--- zoom/vis/sparkline.py
+++ zoom/vis/sparkline.py
@@ -4,10 +4,12 @@
     Sparkline charts library interface
 """
 
-from zoom import system, json
 from uuid import uuid4
 
-scripts = [
+from zoom import system, json
+from zoom.component import component
+
+libs = [
     "/static/dz/jquery/jquery-migrate-1.2.1.min.js",
     "/static/dz/jqsparkline/jquery.sparkline.min.js",
         ]
@@ -17,7 +19,7 @@ js = """
 
         /** This code runs when everything has been loaded on the page */
         /* Inline sparklines take their values from the contents of the tag */
-        $('.inlinesparkline').sparkline('html',<<options>>);
+        $('.inlinesparkline').sparkline('html', <<options>>);
 
         /* Use 'html' instead of an array of values to pass options
         to a sparkline with data in the tag */
@@ -25,6 +27,7 @@ js = """
 
     });
 """
+
 responsive_js = """
     $(function() {
 
@@ -41,23 +44,28 @@ responsive_js = """
     });
 """
 
+
 def chart(selector, data, options):
-    system.libs = system.libs | scripts
-    system.js.add(js.replace('<<options>>',repr(options)))
-    return '<span class="{} inojs">{}</span>'.format(selector, repr(data)[1:-1])
+    return component(
+        '<span class="{} inojs">{}</span>'.format(selector, repr(data)[1:-1]),
+        js=js.replace('<<options>>', repr(options)),
+        libs=libs
+    )
+
 
 def responsive_chart(selector, data, options):
-    system.libs = system.libs | scripts
+    system.libs = system.libs | libs
     script_var = 'spark_%s' % uuid4().hex
     data = json.dumps(data)
     options = json.dumps(options)
     system.js.add(responsive_js % (locals()))
     return '<span class="{} {} inojs">&nbsp;</span>'.format(selector, script_var)
 
+
 def line(data, options={}):
     selector = 'inlinesparkline'
     return chart(selector, data, options)
 
+
 def bar(data, options={}):
     return chart('inlinebar', data, options)
-
